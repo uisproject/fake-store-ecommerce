302a711c03912f6e34264141dd737f3905154bc4
uisproject <uisprojectxyz@gmail.com>
2022-05-16 23:31:33 +0700

    category feature added

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,8 +1,6 @@
 import MainLayout from "@/layouts/MainLayout";
 import AllProducts from "@/components/Homepage/AllProducts";
 import Categories from "@/components/Homepage/Categories";
-import Router from "next/router";
-import { useEffect } from "react";
 
 export default function Home() {
   return (
